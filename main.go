1fc70737e3ee7b5aa7b8fe2973fd52acfc4beb3c
Majid Golshadi <golshadi.majid@gmail.com>
2017-05-16 17:28:32 +0430

    Sync http producer and async log producer

--- main.go
+++ main.go
@@ -2,66 +2,192 @@ package main
 
 import (
 	"github.com/Shopify/sarama"
-	"github.com/prometheus/common/log"
-	"net/http"
-	"time"
+
+	"encoding/json"
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+	"io/ioutil"
 )
 
-type server struct {
-	Producer sarama.SyncProducer
+var (
+	addr      = flag.String("addr", ":8080", "The address to bind to")
+	//brokers   = flag.String("brokers", os.Getenv("KAFKA_PEERS"), "The Kafka brokers to connect to, as a comma separated list")
+	brokers   = flag.String("brokers", "192.168.120.84:9092,192.168.120.85:9092,192.168.120.86:9092", "The Kafka brokers to connect to, as a comma separated list")
+	retry	  = flag.Int("retry", 10, "Retry up to N times to produce the message")
+	zookeeper = flag.String("zookeeper", "192.168.120.84:9092,192.168.120.85:9092,192.168.120.86:9092", "The Kafka brokers to connect to, as a comma separated list")
+	logTopic  = flag.String("logtopic", "kafka_producer_access_log", "kafka topic to produce log in")
+	verbose   = flag.Bool("verbose", false, "Turn on Sarama logging")
+)
+
+func main() {
+	flag.Parse()
+
+	if *verbose {
+		sarama.Logger = log.New(os.Stdout, "[sarama] ", log.LstdFlags)
+	}
+
+	if *brokers == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	brokerList := strings.Split(*brokers, ",")
+	log.Printf("Kafka brokers: %s", strings.Join(brokerList, ", "))
+
+	server := &Server{
+		DataCollector:     newDataCollector(brokerList),
+		AccessLogProducer: newAccessLogProducer(brokerList),
+	}
+	defer func() {
+		if err := server.Close(); err != nil {
+			log.Println("Failed to close server", err)
+		}
+	}()
+
+	log.Fatal(server.Run(*addr))
 }
 
-func (s *server) Handler() http.Handler {
+type Server struct {
+	DataCollector     sarama.SyncProducer
+	AccessLogProducer sarama.AsyncProducer
+}
+
+func (s *Server) Close() error {
+	if err := s.DataCollector.Close(); err != nil {
+		log.Println("Failed to shut down data collector cleanly", err)
+	}
+
+	if err := s.AccessLogProducer.Close(); err != nil {
+		log.Println("Failed to shut down access log producer cleanly", err)
+	}
+
+	return nil
+}
+
+func (s *Server) Handler() http.Handler {
+	return s.withAccessLog(s.collectQueryStringData())
+}
+
+func (s *Server) Run(addr string) error {
+	httpServer := &http.Server{
+		Addr:    addr,
+		Handler: s.Handler(),
+	}
+
+	log.Printf("Listening for requests on %s...\n", addr)
+	return httpServer.ListenAndServe()
+}
+
+func (s *Server) collectQueryStringData() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
+		if !strings.HasPrefix(r.URL.Path, "/topic") {
 			http.NotFound(w, r)
 			return
 		}
 
-		s.Producer.SendMessage(&sarama.ProducerMessage{
-			Topic: "archive-messages",
-			Value: sarama.StringEncoder("ss"),
+		uriArray := strings.Split(r.RequestURI, "/")
+		body, err := ioutil.ReadAll(r.Body);
+		partition, offset, err := s.DataCollector.SendMessage(&sarama.ProducerMessage{
+			Topic: uriArray[len(uriArray) - 1],
+			Value: sarama.StringEncoder(body),
 		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Failed to store your data:, %s", err)
+		} else {
+			fmt.Fprintf(w, "Your data is stored with unique identifier important/%d/%d", partition, offset)
+		}
 	})
 }
 
-func (s *server) Close() {
-	s.Producer.Close()
+type accessLogEntry struct {
+	Method       string  `json:"method"`
+	Host         string  `json:"host"`
+	Path         string  `json:"path"`
+	IP           string  `json:"ip"`
+	ResponseTime float64 `json:"response_time"`
+
+	encoded []byte
+	err     error
 }
 
-func main() {
-	server := &server{}
-	var err error
+func (ale *accessLogEntry) ensureEncoded() {
+	if ale.encoded == nil && ale.err == nil {
+		ale.encoded, ale.err = json.Marshal(ale)
+	}
+}
+
+func (ale *accessLogEntry) Length() int {
+	ale.ensureEncoded()
+	return len(ale.encoded)
+}
+
+func (ale *accessLogEntry) Encode() ([]byte, error) {
+	ale.ensureEncoded()
+	return ale.encoded, ale.err
+}
+
+func (s *Server) withAccessLog(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		started := time.Now()
+
+		next.ServeHTTP(w, r)
+
+		entry := &accessLogEntry{
+			Method:       r.Method,
+			Host:         r.Host,
+			Path:         r.RequestURI,
+			IP:           r.RemoteAddr,
+			ResponseTime: float64(time.Since(started)) / float64(time.Second),
+		}
+
+		s.AccessLogProducer.Input() <- &sarama.ProducerMessage{
+			Topic: *logTopic,
+			Value: entry,
+		}
+	})
+}
+
+func newDataCollector(brokerList []string) sarama.SyncProducer {
+
 	config := sarama.NewConfig()
-
 	config.Producer.RequiredAcks = sarama.WaitForLocal
-	config.Producer.Compression = sarama.CompressionGZIP
-	brokerList := []string{"192.168.120.84:9092", "192.168.120.85:9092", "192.168.120.86:9092"}
+	config.Producer.Retry.Max = *retry
 
-	server.Producer, err = sarama.NewSyncProducer(brokerList , config)
+	producer, err := sarama.NewSyncProducer(brokerList, config)
 	if err != nil {
 		log.Fatalln("Failed to start Sarama producer:", err)
 	}
 
-	// Kafka critical error happening
-	//go func() {
-	//	for err := range server.Producer.Errors() {
-	//		println("Failed to write access log entry:", err)
-	//	}
-	//}()
-
-	s := &http.Server{
-		Addr:           ":8080",
-		Handler:        server.Handler(),
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	defer server.Close()
-
-	fmt.Printf("Listening for requests on %s...\n", s.Addr)
-	s.ListenAndServe()
+	return producer
 }
 
+func newAccessLogProducer(brokerList []string) sarama.AsyncProducer {
+
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForLocal       // Only wait for the leader to ack
+	config.Producer.Compression = sarama.CompressionSnappy
+	config.Producer.Flush.Frequency = 500 * time.Millisecond
+
+	producer, err := sarama.NewAsyncProducer(brokerList, config)
+	if err != nil {
+		log.Fatalln("Failed to start Sarama producer:", err)
+	}
+
+	// Log to STDOUT if app not able to produce messages.
+	// Note: messages will only be returned here after all retry attempts are exhausted.
+	go func() {
+		for err := range producer.Errors() {
+			log.Println("Failed to write access log entry:", err)
+		}
+	}()
+
+	return producer
+}
